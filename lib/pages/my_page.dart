77464a790306883fc5fa2d02ddd3cff44297a5a7
yohei inokuchi <inoyoh@gmail.com>
2024-07-20 20:34:28 +0300

    投稿、フォロワー、フォロー数のスタイル調整

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -7,37 +7,61 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('マイページ')),
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Image.network(
-                'https://static.vecteezy.com/system/resources/previews/018/930/691/non_2x/instagram-logo-instagram-icon-transparent-free-png.png',
-                width:60,
-                height:60,
-              ),
-              Column(
-                children: [
-                  Text('7,041'),
-                  Text('投稿'),
-                ],
-              ),
-              Column(
-                children: [
-                  Text('4.6億'),
-                  Text('フォロワー'),
-                ],
-              ),
-              Column(
-                children: [
-                  Text('99'),
-                  Text('フォロー中'),
-                ],
-              ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Image.network(
+                  'https://static.vecteezy.com/system/resources/previews/018/930/691/non_2x/instagram-logo-instagram-icon-transparent-free-png.png',
+                  width:80,
+                  height:80,
+                ),
+                Spacer(), // スペースを開ける
+                Column(
+                  children: [
+                    Text(
+                      '7,041',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                      ),
+                    ),
+                    Text('投稿'),
+                  ],
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  children: [
+                    Text(
+                      '4.6億',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text('フォロワー'),
+                  ],
+                ),
+                const SizedBox(width: 16),
+                Column(
+                  children: [
+                    Text(
+                      '99',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Text('フォロー中'),
+                  ],
+                ),
 
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
