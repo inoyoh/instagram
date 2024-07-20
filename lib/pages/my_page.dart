95b8e1cd3a594293f56a5038389e18b18cb8453b
yohei inokuchi <inoyoh@gmail.com>
2024-07-20 20:26:08 +0300

    アイコン、投稿数、フォロワー、フォロー中の表示

--- lib/pages/my_page.dart
+++ lib/pages/my_page.dart
@@ -7,6 +7,38 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('マイページ')),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Image.network(
+                'https://static.vecteezy.com/system/resources/previews/018/930/691/non_2x/instagram-logo-instagram-icon-transparent-free-png.png',
+                width:60,
+                height:60,
+              ),
+              Column(
+                children: [
+                  Text('7,041'),
+                  Text('投稿'),
+                ],
+              ),
+              Column(
+                children: [
+                  Text('4.6億'),
+                  Text('フォロワー'),
+                ],
+              ),
+              Column(
+                children: [
+                  Text('99'),
+                  Text('フォロー中'),
+                ],
+              ),
+
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
