e47bf2d94018420e1ca3c052a97d340c39be527d
yohei inokuchi <inoyoh@gmail.com>
2024-07-20 14:10:12 +0300

    不必要なコード削除（counter）

--- lib/main.dart
+++ lib/main.dart
@@ -32,13 +32,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,10 +46,6 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             const Text(
               'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
